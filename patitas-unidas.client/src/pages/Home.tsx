72040bd5ffe8c047f2be32c9ac83db7b9f117a1b
ingjhonhernandezgomez <ing.jhonhernandezgomez@gmail.com>
2025-04-24 21:19:50 -0500

    cambio de import, ruta relativa

--- patitas-unidas.client/src/pages/Home.tsx
+++ patitas-unidas.client/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import "/src/styles/home.css"
+import "../styles/home.css"
 
 export default function Home() {
     const animals = [
