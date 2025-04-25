ac97dc4aa9eeae9d53a6e4ad6e1181def8957bee
ingjhonhernandezgomez <ing.jhonhernandezgomez@gmail.com>
2025-04-24 22:15:29 -0500

    prueba con solo /

--- patitas-unidas.client/vite.config.ts
+++ patitas-unidas.client/vite.config.ts
@@ -39,7 +39,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/patitas-unidas/',
+    base: '/',
     plugins: [react()],
     resolve: {
         alias: {
