10872a6aae1361e0028e5e8321786a7679ed012f
ingjhonhernandezgomez <ing.jhonhernandezgomez@gmail.com>
2025-05-22 22:11:27 -0500

    V1 ajustes a vista de Dona, se agrega ventana de mantenimiento

---
+++ patitas-unidas.client/src/components/Maintenance.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import "/src/styles/NotFound.css"
+
+export default function Maintenance() {
+  return (
+    <div className="not-found" style={{ position: 'relative' }}>
+      <div className="not-found-content">
+        <i className="icon-paw"></i>
+        <h1>¡Estamos mejorando esta sección!</h1>
+        <p>
+          Pronto podrás ayudar a más peluditos y fundaciones desde aquí. Mientras tanto, sigue explorando y cambiando vidas.
+        </p>
+        <Link to="/patitas-unidas" className="button primary">
+          Volver al inicio
+        </Link>
+      </div>
+      <div className="paw-prints">
+        <span className="icon-paw paw-print"></span>
+        <span className="icon-paw paw-print"></span>
+        <span className="icon-paw paw-print"></span>
+        <span className="icon-paw paw-print"></span>
+      </div>
+    </div>
+  )
+} 
